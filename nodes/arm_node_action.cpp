f4798cfd73be3a6ecbd33a3fe7fc8152b3c93818
Matthew Tesch <matt@hebirobotics.com>
2019-02-11 12:53:38 -0500

    Condense the `arm_node` and `arm_node_action` node

--- nodes/arm_node_action.cpp
+++ nodes/arm_node_action.cpp
@@ -1,71 +1,57 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Point.h>
-#include <hebi_cpp_api_examples/TargetWaypoints.h>
+#include <ros/console.h>
+#include <ros/package.h>
 
+#include <actionlib/server/simple_action_server.h>
+
+#include <geometry_msgs/Point.h>
+
+#include <hebi_cpp_api_examples/TargetWaypoints.h>
 #include <hebi_cpp_api_examples/ArmMotionAction.h>
 
-#include "actionlib/server/simple_action_server.h"
+#include "group_command.hpp"
 
 #include "robot/arm.hpp"
 #include "robot_model.hpp"
-#include "group_command.hpp"
-
-#include <ros/console.h>
-#include <ros/package.h>
 
 namespace hebi {
 namespace ros {
 
 class ArmNode {
 public:
-  ArmNode(arm::Arm& arm)
-    : arm_(arm)
-  {
-  }
+  ArmNode(arm::Arm& arm) : arm_(arm) { }
 
-  void startArmMotion(const hebi_cpp_api_examples::ArmMotionGoalConstPtr& goal)
-  {
+  // Replan a smooth joint trajectory from the current location through a
+  // series of cartesian waypoints.
+  // xyz positions should be a 3xn vector of target positions
+  void updateCartesianWaypoints(const Eigen::Matrix3Xd& xyz_positions,
+      const Eigen::Matrix3Xd& end_tip_directions) {
+    // Data sanity check:
+    if (end_tip_directions.cols() > 0 && end_tip_directions.cols() != xyz_positions.cols())
+      return;
 
-    // Replan a smooth joint trajectory from the current location through a
-    // series of cartesian waypoints.
-    // TODO: use a single struct instead of 6 single vectors of the same length;
-    // but how do we do hierarchial actions?
-    size_t num_waypoints = goal->x.size();
+    // Update stored target position:
+    target_xyz_ = xyz_positions.col(xyz_positions.cols() - 1);
 
     // These are the joint angles that will be added
+    auto num_waypoints = xyz_positions.cols();
     Eigen::MatrixXd positions(arm_.size(), num_waypoints);
 
     // Plan to each subsequent point from the last position
-    // Eigen::VectorXd last_position = arm_.getLastFeedback().getPosition();
+    // (We use the last position command for smoother motion)
     Eigen::VectorXd last_position = arm_.getLastFeedback().getPositionCommand();
 
     // Get joint angles to move to each waypoint
     for (size_t i = 0; i < num_waypoints; ++i) {
 
-      Eigen::Vector3d xyz;
-      Eigen::Vector3d end_tip;
-
-      // Special homing values...
-      if (goal->x[i] == 100 && goal->y[i] == 100 && goal->z[i] == 100) {
-        xyz = arm_.getHomePositionXYZ();
-        end_tip << 1, 0, 0;
-      } 
-
-      else if (goal->x[i] == 101 && goal->y[i] == 101 && goal->z[i] == 101) {
-        xyz = arm_.getHomePositionXYZ();
-        end_tip << 0, 0, -1;
-      } 
-
-      else {
-        xyz << goal->x[i], goal->y[i], goal->z[i];
-        end_tip << goal->tipx[i], goal->tipy[i], goal->tipz[i];
-      }
-
       // Find the joint angles for the next waypoint, starting from the last
-      // waypoint
-      last_position = arm_.getKinematics().solveIK(last_position, xyz, end_tip);
-
-      // Save the waypoints
+      // waypoint, and save into the position vector.
+      if (end_tip_directions.cols() > 0) {
+        // If we are given tip directions, add these too...
+        last_position = arm_.getKinematics().solveIK(last_position, xyz_positions.col(i), end_tip_directions.col(i));
+      } else {
+        last_position = arm_.getKinematics().solveIK(last_position, xyz_positions.col(i));
+      }
       positions.col(i) = last_position; 
     }
 
@@ -74,6 +60,109 @@ public:
       ::ros::Time::now().toSec(),
       arm_.getLastFeedback(),
       positions);
+  }
+
+  void updateCartesianWaypoints(hebi_cpp_api_examples::TargetWaypoints target_waypoints) {
+    action_server_->setAborted();
+
+    // Fill in an Eigen::Matrix3xd with the xyz goal
+    size_t num_waypoints = target_waypoints.waypoints_vector.size();
+    Eigen::Matrix3Xd xyz_waypoints(3, num_waypoints);
+    for (size_t i = 0; i < num_waypoints; ++i) {
+      const auto& xyz_waypoint = target_waypoints.waypoints_vector[i];
+      xyz_waypoints(0, i) = xyz_waypoint.x;
+      xyz_waypoints(1, i) = xyz_waypoint.y;
+      xyz_waypoints(2, i) = xyz_waypoint.z;
+    }
+
+    // Replan
+    Eigen::Matrix3Xd tip_directions(3, 0);
+    updateCartesianWaypoints(xyz_waypoints, tip_directions);
+  }
+
+  // Set the target end effector location in (x,y,z) space, replanning
+  // smoothly to the new location
+  void setTargetCallback(geometry_msgs::Point data) {
+    action_server_->setAborted();
+
+    // Fill in an Eigen::Matrix3xd with the xyz goal
+    Eigen::Matrix3Xd xyz_waypoints(3, 1);
+    xyz_waypoints(0, 0) = data.x;
+    xyz_waypoints(1, 0) = data.y;
+    xyz_waypoints(2, 0) = data.z;
+
+    // Replan
+    Eigen::Matrix3Xd tip_directions(3, 0);
+    updateCartesianWaypoints(xyz_waypoints, tip_directions);
+  }
+
+  // "Jog" the target end effector location in (x,y,z) space, replanning
+  // smoothly to the new location
+  void offsetTargetCallback(geometry_msgs::Point data) {
+    action_server_->setAborted();
+
+    // Only update if target changes!
+    if (data.x == 0 && data.y == 0 && data.z == 0)
+      return;
+
+    // Initialize target from feedback as necessary
+    if (!isTargetInitialized()) {
+      auto pos = arm_.getKinematics().FK(arm_.getLastFeedback().getPositionCommand());
+      target_xyz_.x() = pos.x();
+      target_xyz_.y() = pos.y();
+      target_xyz_.z() = pos.z();
+    }
+
+    // Fill in an Eigen::Matrix3xd with the xyz goal
+    Eigen::Matrix3Xd xyz_waypoints(3, 1);
+    xyz_waypoints(0, 0) = target_xyz_.x() + data.x;
+    xyz_waypoints(1, 0) = target_xyz_.y() + data.y;
+    xyz_waypoints(2, 0) = target_xyz_.z() + data.z;
+
+    // Replan
+    Eigen::Matrix3Xd tip_directions(3, 0);
+    updateCartesianWaypoints(xyz_waypoints, tip_directions);
+  }
+
+  void startArmMotion(const hebi_cpp_api_examples::ArmMotionGoalConstPtr& goal) {
+    ROS_INFO("Executing arm motion action");
+
+    // Replan a smooth joint trajectory from the current location through a
+    // series of cartesian waypoints.
+    // TODO: use a single struct instead of 6 single vectors of the same length;
+    // but how do we do hierarchial actions?
+    size_t num_waypoints = goal->x.size();
+
+    Eigen::Matrix3Xd xyz_positions(3, num_waypoints);
+    Eigen::Matrix3Xd tip_directions(3, num_waypoints);
+
+    // Get joint angles to move to each waypoint
+    for (size_t i = 0; i < num_waypoints; ++i) {
+      // Special homing values...
+      if (goal->x[i] == 100 && goal->y[i] == 100 && goal->z[i] == 100) {
+        xyz_positions.col(i) = arm_.getHomePositionXYZ();
+        tip_directions(0, i) = 1;
+        tip_directions(1, i) = 0;
+        tip_directions(2, i) = 0;
+      }
+      else if (goal->x[i] == 101 && goal->y[i] == 101 && goal->z[i] == 101) {
+        xyz_positions.col(i) = arm_.getHomePositionXYZ();
+        tip_directions(0, i) = 0;
+        tip_directions(1, i) = 0;
+        tip_directions(2, i) = -1;
+      }
+      else {
+        xyz_positions(0, i) = goal->x[i];
+        xyz_positions(1, i) = goal->y[i];
+        xyz_positions(2, i) = goal->z[i];
+        tip_directions(0, i) = goal->tipx[i];
+        tip_directions(1, i) = goal->tipy[i];
+        tip_directions(2, i) = goal->tipz[i];
+      }
+    }
+
+    updateCartesianWaypoints(xyz_positions, tip_directions);
+
     Color color;
     if (goal->set_color)
       color = Color(goal->r, goal->g, goal->b, 255);
@@ -82,18 +171,23 @@ public:
     // Wait until the action is complete, sending status/feedback along the
     // way.
     ::ros::Rate r(10);
-    bool success = true;
-
-    ROS_INFO("Executing arm motion action");
 
     hebi_cpp_api_examples::ArmMotionFeedback feedback;
 
     while (true) {
       if (action_server_->isPreemptRequested() || !::ros::ok()) {
-        ROS_INFO("Preempted arm motion");
+        ROS_INFO("Arm motion action was preempted");
+        arm_.clearColor();
+        // Note -- the `startArmMotion` function will not be called until the
+        // action server has been preempted here:
         action_server_->setPreempted();
-        success = false;
-        break;
+        return;
+      }
+
+      if (!action_server_->isActive() || !::ros::ok()) {
+        ROS_INFO("Arm motion was cancelled");
+        arm_.clearColor();
+        return;
       }
 
       auto t = ::ros::Time::now().toSec();
@@ -104,7 +198,6 @@ public:
       action_server_->publishFeedback(feedback);
  
       if (arm_.isTrajectoryComplete(t)) {
-        ROS_INFO("COMPLETE");
         break;
       }
 
@@ -116,9 +209,7 @@ public:
 
     // publish when the arm is done with a motion
     ROS_INFO("Completed arm motion action");
-    hebi_cpp_api_examples::ArmMotionResult result;
-    result.success = success;
-    action_server_->setSucceeded(result);
+    action_server_->setSucceeded();
   }
 
   void setActionServer(actionlib::SimpleActionServer<hebi_cpp_api_examples::ArmMotionAction>* action_server) {
@@ -128,8 +219,20 @@ public:
 private:
   arm::Arm& arm_;
 
-  actionlib::SimpleActionServer<hebi_cpp_api_examples::ArmMotionAction>* action_server_ {nullptr};
+  // The end effector location that this arm will target (NaN indicates
+  // unitialized state, and will be set from feedback during first offset)
+  Eigen::Vector3d target_xyz_{
+    std::numeric_limits<double>::quiet_NaN(),
+    std::numeric_limits<double>::quiet_NaN(),
+    std::numeric_limits<double>::quiet_NaN()};
+  bool isTargetInitialized() {
+    return !std::isnan(target_xyz_.x()) ||
+           !std::isnan(target_xyz_.y()) ||
+           !std::isnan(target_xyz_.z());
+  }
 
+
+  actionlib::SimpleActionServer<hebi_cpp_api_examples::ArmMotionAction>* action_server_ {nullptr};
 };
 
 } // namespace ros
@@ -266,18 +369,25 @@ int main(int argc, char ** argv) {
 
   arm_motion_action.start();
 
-  /////////////////// Main Loop ///////////////////
+  // "Jog" the end effector
+  ros::Subscriber offset_target_subscriber =
+    node.subscribe<geometry_msgs::Point>("offset_target", 50, &hebi::ros::ArmNode::offsetTargetCallback, &arm_node);
 
-  double t_now;
+  // Explicitly set the end effector's target position
+  ros::Subscriber set_target_subscriber =
+    node.subscribe<geometry_msgs::Point>("set_target", 50, &hebi::ros::ArmNode::setTargetCallback, &arm_node);
+
+  // Subscribe to lists of (x, y, z) waypoints
+  ros::Subscriber waypoint_subscriber =
+    node.subscribe<hebi_cpp_api_examples::TargetWaypoints>("cartesian_waypoints", 50, &hebi::ros::ArmNode::updateCartesianWaypoints, &arm_node);
+
+  /////////////////// Main Loop ///////////////////
 
   // Main command loop
   while (ros::ok()) {
-
-    auto t = ros::Time::now().toSec();
-
     // Update feedback, and command the arm to move along its planned path
     // (this also acts as a loop-rate limiter so no 'sleep' is needed)
-    if (!arm->update(t))
+    if (!arm->update(ros::Time::now().toSec()))
       ROS_WARN("Error Getting Feedback -- Check Connection");
 
     // Call any pending callbacks (note -- this may update our planned motion)
