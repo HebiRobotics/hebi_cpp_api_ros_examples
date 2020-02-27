765d11937ef59ca5675480c14a2a93ba6e9ad0d9
Chris Bollinger <chris@hebirobotics.com>
2020-02-26 20:51:21 -0500

    First pass for Mecanum base ROS node. Only cmd_vel twist tested

---
+++ src/kits/base/mecanum_base_node.cpp
@@ -0,0 +1,206 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Point.h>
+
+#include <hebi_cpp_api_examples/BaseMotionAction.h>
+
+#include "actionlib/server/simple_action_server.h"
+
+#include "geometry_msgs/Twist.h"
+
+#include "hebi_cpp_api/lookup.hpp"
+#include "hebi_cpp_api/group.hpp"
+#include "hebi_cpp_api/group_command.hpp"
+#include "hebi_cpp_api/group_feedback.hpp"
+#include "hebi_cpp_api/trajectory.hpp"
+
+#include "src/util/mecanum_base.hpp"
+
+#include <ros/console.h>
+#include <ros/package.h>
+
+namespace hebi {
+namespace ros {
+
+class BaseNode {
+public:
+  BaseNode(MecanumBase& base) : base_(base) {
+    Color c;
+    base_.clearColor();
+  }
+
+  void twistCallback(geometry_msgs::Twist msg) {
+    if(action_server_->isActive()) {
+      ROS_INFO("Canceling active trajectory due to twist msg");
+      trajectory_canceled_ = true;
+    }
+    base_.directVelocityControl(msg.linear.x, msg.linear.y, msg.angular.z);
+  }
+
+  // Returns "false" if we have error...
+  bool publishActionProgress(float start_prog, float end_prog)
+  {
+    trajectory_canceled_ = false;
+    hebi_cpp_api_examples::BaseMotionFeedback feedback;
+    ::ros::Rate r(10);
+
+    // Wait until the action is complete, sending status/feedback along the way.
+    while (true) {
+      if (action_server_->isPreemptRequested() || trajectory_canceled_ || !::ros::ok()) {
+        ROS_INFO("Preempted base motion");
+        action_server_->setPreempted();
+        return false;
+      }
+      auto t = ::ros::Time::now().toSec();
+
+      // Publish progress:
+      feedback.percent_complete = start_prog + (end_prog - start_prog) * base_.trajectoryPercentComplete(t) / 2.0;
+      action_server_->publishFeedback(feedback);
+ 
+      if (base_.isTrajectoryComplete(t)) {
+        break;
+      }
+
+      // Limit feedback rate
+      r.sleep(); 
+    }
+    return true;
+
+  }
+
+  void startBaseMotion(const hebi_cpp_api_examples::BaseMotionGoalConstPtr& goal) {
+
+    ROS_INFO("Executing base motion action");
+    // Note: this is implemented right now as rotation, _THEN_ translation, _THEN_ rotation...
+
+    // Set color:
+    Color color;
+    if (goal->set_color)
+      color = Color(goal->r, goal->g, goal->b, 255);
+    base_.setColor(color);
+
+    // Face towards (x, y), assuming we start pointed with the x axis forward and the y axis left:
+    bool success = true;
+    if (goal->x != 0 || goal->y != 0)
+    {
+      auto theta_dir = std::atan2(goal->y, goal->x);
+      auto goal_dist = std::sqrt(goal->x * goal->x + goal->y * goal->y);
+
+      base_.startRotateBy(theta_dir,
+          ::ros::Time::now().toSec());
+      success = publishActionProgress(0.0, 0.33);
+      if (success) {
+        base_.startMoveForward(goal_dist,
+          ::ros::Time::now().toSec());
+        success = publishActionProgress(0.33, 0.67);
+      }
+      if (success) {
+        base_.startRotateBy(goal->theta - theta_dir,
+          ::ros::Time::now().toSec());
+        success = publishActionProgress(0.67, 1.0);
+      }
+    } else {
+      // Pure rotation:
+      base_.startRotateBy(goal->theta,
+          ::ros::Time::now().toSec());
+      success = publishActionProgress(0.0, 1.0);
+    }
+
+    base_.clearColor();
+
+    // publish when the base is done with a motion
+    ROS_INFO("Completed base motion action");
+
+    hebi_cpp_api_examples::BaseMotionResult result;
+
+    result.success = success;
+    action_server_->setSucceeded(result); // TODO: set failed?
+  }
+
+  void setActionServer(actionlib::SimpleActionServer<hebi_cpp_api_examples::BaseMotionAction>* action_server) {
+    action_server_ = action_server;
+  }
+
+private:
+  MecanumBase& base_; 
+
+  actionlib::SimpleActionServer<hebi_cpp_api_examples::BaseMotionAction>* action_server_ {nullptr};
+  bool trajectory_canceled_ = false;
+};
+
+} // namespace ros
+} // namespace hebi
+
+int main(int argc, char ** argv) {
+
+  // Initialize ROS node
+  ros::init(argc, argv, "diff_drive_node");
+  ros::NodeHandle node;
+
+  // Get parameters for name/family of modules; default to standard values:
+  std::vector<std::string> families;
+  if (node.hasParam("families") && node.getParam("families", families)) {
+    ROS_INFO("Found and successfully read 'families' parameter");
+  } else {
+    ROS_INFO("Could not find/read 'families' parameter; defaulting to 'MecanumBase'");
+    families = {"mecanumBase"};
+  }
+
+  std::vector<std::string> names;
+  if (node.hasParam("names") && node.getParam("names", names)) {
+    ROS_INFO("Found and successfully read 'names' parameter");
+  } else {
+    ROS_INFO("Could not find/read 'names' parameter; defaulting to 'front/backLeft' and 'front/backRight'");
+    names = {"frontLeft", "backLeft", "frontRight", "backRight"};
+  }
+
+  /////////////////// Initialize base ///////////////////
+
+  // Create base and plan initial trajectory
+  std::string error_out;
+  auto base = hebi::MecanumBase::create(
+    families, // Famil(ies)
+    names, // Names
+    ::ros::package::getPath("hebi_cpp_api_examples") + "/config/gains/mecanum_base_gains.xml", // Gains file
+    ros::Time::now().toSec(), // Starting time (for trajectory)
+    error_out);
+  if (!base) {
+    ROS_ERROR_STREAM(error_out);
+    ROS_ERROR("Could not initialize base! Check for modules on the network, and ensure good connection (e.g., check packet loss plot in Scope). Shutting down...");
+    return -1;
+  }
+
+  /////////////////// Initialize ROS interface ///////////////////
+   
+  hebi::ros::BaseNode base_node(*base);
+
+  auto sub_handle = node.subscribe("cmd_vel", 10, &hebi::ros::BaseNode::twistCallback, &base_node);
+
+  // Action server for base motions
+  actionlib::SimpleActionServer<hebi_cpp_api_examples::BaseMotionAction> base_motion_action(
+    node, "motion",
+    boost::bind(&hebi::ros::BaseNode::startBaseMotion, &base_node, _1), false);
+
+  base_node.setActionServer(&base_motion_action);
+
+  base_motion_action.start();
+
+  /////////////////// Main Loop ///////////////////
+
+  double t_now;
+
+  // Main command loop
+  while (ros::ok()) {
+
+    auto t = ros::Time::now().toSec();
+
+    // Update feedback, and command the base to move along its planned path
+    // (this also acts as a loop-rate limiter so no 'sleep' is needed)
+    if (!base->update(t))
+      ROS_WARN("Error Getting Feedback -- Check Connection");
+
+    // Call any pending callbacks (note -- this may update our planned motion)
+    ros::spinOnce();
+  }
+
+  return 0;
+}
